9ad7842118fdbc498272a5befc780f70eaf46930
İbrahim TURAN <ibrahimturan002@gmail.com>
2020-02-09 14:30:25 +0300

    config and secret files deleted

--- config/database.js
+++
@@ -1,7 +0,0 @@
-const sequelize = require('sequelize');
-
-//DB CONNECTION
-module.exports = new sequelize('vqdswjru','vqdswjru', 'AtvHh4rl-Yq6AcR-VgP1SDkAq-6qaNXw', {
-    host: 'manny.db.elephantsql.com',
-    dialect: 'postgres',
-});
